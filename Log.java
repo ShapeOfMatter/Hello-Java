4a6aca4abe740981d026308b8b356b147d2e871c
ShapeOfMatter <mako@pop-os.laptop>
2019-10-30 13:38:11 -0400

    seperate log handler

---
+++ Log.java
@@ -0,0 +1,7 @@
+public class Log {
+
+    public static void log(String s) {
+        System.out.println(s);
+    }
+
+}
--- Main.java
+++ Main.java
@@ -9,16 +9,15 @@ import com.sun.net.httpserver.HttpContext;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        //test the DB connection
         SQLite.connect();
+        
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         HttpContext context = server.createContext("/", new MyHandler());
-        log(context.getPath());
+        Log.log(context.getPath());
+        
         server.setExecutor(null); // creates a default executor
         server.start();
-    }
-
-    public static void log(String s) {
-        System.out.println(s);
     }
 
 }
