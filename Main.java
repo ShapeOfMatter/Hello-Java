c7fa056427e3d87f312ad255d37fc4411b745cff
ShapeOfMatter <mako@pop-os.laptop>
2019-10-30 12:42:21 -0400

    querystring inspection is working

--- Main.java
+++ Main.java
@@ -1,20 +1,25 @@
 import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpContext;
 
 //import com.MyHandler;
 
 /*
  * a simple static http server
-*/
+ */
 public class Main {
 
-  public static void main(String[] args) throws Exception {
-    HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-    server.createContext("/test", new MyHandler());
-    server.setExecutor(null); // creates a default executor
-    server.start();
-  }
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        HttpContext context = server.createContext("/", new MyHandler());
+        log(context.getPath());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
 
-  
+    public static void log(String s) {
+        System.out.println(s);
+    }
+
 }
--- MyHandler.java
+++ MyHandler.java
@@ -1,12 +1,15 @@
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class MyHandler implements HttpHandler {
     public void handle(HttpExchange t) throws IOException {
-        byte [] response = "Welcome Real's HowTo test page".getBytes();
+        URI request = t.getRequestURI();
+        String query = request.getQuery();
+        byte [] response = String.format("You requested \"%s\"", query).getBytes();
         t.sendResponseHeaders(200, response.length);
         OutputStream os = t.getResponseBody();
         os.write(response);
